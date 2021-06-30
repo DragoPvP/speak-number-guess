8c66a69769bd8c727935607536c25892622f09c6
Hesham-Khan <heshamkhan132@gmail.com>
2021-06-30 17:40:48 +0530

    SR service added

--- script.js
+++ script.js
@@ -15,8 +15,46 @@ recognition.start();
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
 
-//  writeMessage(msg);
-//  checkNumber(msg);
+ writeMessage(msg);
+ checkNumber(msg);
+}
+
+// Write what user speaks
+function writeMessage(msg) {
+    msgEl.innerHTML = `
+    <div>You Said: </div>
+    <span class="box">${msg}</span>
+    `;
+}
+
+// Check message against number
+function checkNumber(msg) {
+    const num = +msg;
+
+    // Check if valid number
+    if (Number.isNaN(num)) {
+        msgEl.innerHTML = '<div>That is not a valid number!</div>'
+        return;
+    }
+
+    // Check in range
+    if (num > 100 || num < 1) {
+        msgEl.innerHTML ='<div>Number must be between 1 and 100...';
+        return;
+    }
+
+    // Check Number
+    if (num === randomNum) {
+        document.body.innerHTML = `
+            <h2>Congrats! You have guessed the number! <br><br>
+            It was ${num}</h2>
+            <button class="play-again" id="play-again">Play Again</button>
+        `;
+    } else if (num > randomNum) {
+        msgEl.innerHTML += '<div>Go Lower!</div>'
+    } else {
+        msgEl.innerHTML += '<div>Go Higher!</div>'
+    }
 }
 
 // Generate random number
@@ -25,4 +63,7 @@ function getRandomNumber() {
   }
 
 // Speak result
-recognition.addEventListener('result', onSpeak);
+recognition.addEventListener('result', onSpeak);;
+
+// End SR service
+recognition.addEventListener('end', () => { recognition.start() });
